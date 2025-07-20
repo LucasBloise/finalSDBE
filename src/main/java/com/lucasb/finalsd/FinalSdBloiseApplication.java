12298c4f6190aa003f8a2b0d6c504321a1a0222b
Lucas Bloise <lucasbloise1@gmail.com>
2025-07-20 19:59:11 -0300

    Init Project

---
+++ src/main/java/com/lucasb/finalsd/FinalSdBloiseApplication.java
@@ -0,0 +1,13 @@
+package com.lucasb.finalsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSdBloiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSdBloiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasb/finalsd/FinalSdBloiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasb.finalsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSdBloiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
